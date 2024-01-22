916d4942e45a7468e26892e6bd2875214dea333a
Tomasz Krasieńko <tomaszkrasienko@Tomaszs-MacBook-Pro.local>
2024-01-22 18:40:26 +0100

    Added api project

---
+++ e-Commerce.WebHooks/src/e-Commerce.WebHooks.Api/Configuration/Extensions.cs
@@ -0,0 +1,7 @@
+namespace e_Commerce.WebHooks.Api.Configuration;
+
+public static class Extensions
+{
+    public static IServiceCollection AddConfiguration(this IServiceCollection services)
+        => services;
+}
